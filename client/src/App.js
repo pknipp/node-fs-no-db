8883810b494e51ffeb4149bf21ed81ebcf5c9177
Peter Knipp <peteraknipp@gmail.com>
2022-03-12 12:39:39 -0500

    Remove Cookies import.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Cookies from "js-cookie";
 import Home from './components/Home';
 
 const App = () => {
-  const [fetchWithCSRF] = useState(() => fetch);
-  const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true)
 
   return (
